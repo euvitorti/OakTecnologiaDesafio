476f639326c509cd50691ff8a6da079e97181ac5
Vítor Santos <jvitor.ti03@gmail.com>
2024-10-02 20:04:10 -0300

    Update TokenJwt.java

--- OakTecnologia/OakTecnologia/src/main/java/com/oak/tecnologia/OakTecnologia/infra/TokenJwt.java
+++ OakTecnologia/OakTecnologia/src/main/java/com/oak/tecnologia/OakTecnologia/infra/TokenJwt.java
@@ -4,17 +4,12 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
 import com.auth0.jwt.exceptions.JWTVerificationException;
-import com.auth0.jwt.exceptions.TokenExpiredException;
-import com.auth0.jwt.interfaces.Claim;
 import com.oak.tecnologia.OakTecnologia.models.user.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
-import java.util.Map;
-import java.util.function.Function;
 
 @Service
 public class TokenJwt {
@@ -55,34 +50,6 @@ public class TokenJwt {
         } catch (JWTVerificationException exception) {
             // Invalid signature/claims
             throw new RuntimeException("Token inválido ou expirado!");
-        }
-    }
-
-
-    public <T> T getClaim(String token, Function<Map<String, Claim>, T> claimsResolver) {
-        Map<String, Claim> claims = getAllClaims(token);
-        return claimsResolver.apply(claims);
-    }
-
-    public String getUsername(String token) {
-        return getClaim(token, claims -> claims.get("sub").asString());
-    }
-
-    public Map<String, Claim> getAllClaims(String token) {
-        var algorithm = Algorithm.HMAC256(secret);
-        return JWT.require(algorithm)
-                .withIssuer("API Chat")
-                .build()
-                .verify(token)
-                .getClaims();
-    }
-
-
-    public boolean isTokenValid(String token, UserDetails userDetails) {
-        try {
-            return getSubject(token).equals(userDetails.getUsername());
-        } catch (TokenExpiredException ex) {
-            return false;
         }
     }
 
